aac0ffe6d9de2dd77503a2dc0b0853ca0dbb6a8d
Sergey <79244748+Hambar17umyan@users.noreply.github.com>
2024-08-28 11:51:40 +0400

    Repo replacement

---
+++ BookStore Application/Admin Libriary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Admin_Libriary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ BookStore Application/Admin Service/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Admin_Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
